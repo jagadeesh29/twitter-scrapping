9a3447ca946baaa8f12e0d2aa5dca47b60948c57
jagadeesh <36129585+jagadeesh29@users.noreply.github.com>
2018-04-02 19:50:11 +0530

    Update twiiter.py

--- twiiter.py
+++ twiiter.py
@@ -1,26 +1,22 @@
-# -*- coding: utf-8 -*-
 """
-Created on Sun Apr  1 12:48:25 2018
-
 @author: jac
 """
 
 import tweepy
 import textblob
-consumer_key = "OmDCSvdinB9K3XupAJU5ypPyz"
-consumer_secret = "s4CInjNtik2zMCakefg1QJuTjF2AYKxBtyZFr6UYZrZXxGd1h6"
-access_token = "946017716485341184-3dOAZH3Dy7Fr6lKp5HJU8kvRYqH8GKd"
-access_token_secret = "RWQUthdneQPmaCRXKNz2dIvnXn1fVn6kIIjKZXo9o8bGM"
+# you can add your own key information.
+consumer_key = "xxxxxxxxxxxxxxxxxxx"
+consumer_secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+access_token = "9xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+access_token_secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
-
 auth.set_access_token(access_token,access_token_secret)
 api = tweepy.API(auth)
 
 #get my timeline mes by using home_timeline
 
 public_tweets = api.home_timeline()
-
 for tweet in public_tweets:
     print (tweet.text)
     
@@ -35,7 +31,6 @@ for tweet in result:
 query = "#NammavarAgainstSterlite"
 # Language code (follows ISO 639-1 standards)
 language = "en"
-
 # Calling the user_timeline function with our parameters
 results = api.search(q=query, lang=language)
 
